5a3f86b03ae5cb48c16ad6ad70f55aa0188e322a
Idun420 <74796878+Idun420@users.noreply.github.com>
2021-01-11 21:13:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 def genarateBar(xp, level):
     req = level * 25 + 100 # the required xp for the next level update this for your use case
-    proc = xp / req * 100 # The maount of procent left for the next level
+    proc = xp / req * 100 # The amount of procent left for the next level
 
     proc *= 4 # The canvas is 400 pixels long since proc is a number between 0 and 100 we * it by 4 to get the amount of pixels to draw
     im = Image.new('RGBA', (400, 30), (0, 0, 0, 0)) # Create the canvas
@@ -15,7 +15,7 @@ def genarateBar(xp, level):
     TINT_COLOR = (0, 0, 0)  # Setup for transparency
     TRANSPARENCY = .10  # Degree of transparency, 0-100%
     OPACITY = int(255 * TRANSPARENCY)
-    draw.rectangle(((0, 16), (600, 25)), fill=TINT_COLOR+(OPACITY,)) # Draw the trasparent line under the leveling bar
+    draw.rectangle(((0, 16), (600, 25)), fill=TINT_COLOR+(OPACITY,)) # Draw the transparent line under the leveling bar
 
 
     draw.line((0, 20, proc, 20), fill=(255, 0, 61), width=5) # Draw the leveling bar
